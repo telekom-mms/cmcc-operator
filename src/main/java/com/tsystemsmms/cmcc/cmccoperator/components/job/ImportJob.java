1c651cd8f22d201be10b82860bcb17787bcbdb85
Stefan Bethke <stefan.bethke@t-systems.com>
2024-06-04 16:42:04 +0200

    Add management tools cron job
    With the cron job component, you can run mangement tools commands on a schedule, for example to empty the recycle bin or expire old versions.

--- src/main/java/com/tsystemsmms/cmcc/cmccoperator/components/job/ImportJob.java
+++ src/main/java/com/tsystemsmms/cmcc/cmccoperator/components/job/ImportJob.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022. T-Systems Multimedia Solutions GmbH
+ * Copyright (c) 2022-2024. T-Systems Multimedia Solutions GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
@@ -8,9 +8,10 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-package com.tsystemsmms.cmcc.cmccoperator.crds;
+package com.tsystemsmms.cmcc.cmccoperator.components.job;
 
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.tsystemsmms.cmcc.cmccoperator.crds.UsernamePasswordSecretRef;
 import io.fabric8.kubernetes.api.model.EnvVar;
 import lombok.Data;
 
